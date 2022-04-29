aa1a230240056ba071a956a9120b843068d78c52
vanillaen <vanillaen@yandex.ru>
2022-04-29 09:26:30 +0300

    stable

--- src/components/PartnerContacts/PartnerContacts.js
+++ src/components/PartnerContacts/PartnerContacts.js
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux"
 import PartnerRightColHeading from "../PartnerRightColHeading/PartnerRightColHeading"
 import PartnerInfoContainer from "../PartnerInfoContainer/PartnerInfoContainer"
 import PartnersTwoColumns from "../PartnersTwoColumns/PartnersTwoColumns"
-import PartnerInput from "../PartnerInput/PartnerInput"
 import PartnerInputWithAdding from "../PartnerInput/PartnerInputWithAdding"
 
 function PartnerContacts() {
@@ -16,6 +15,7 @@ function PartnerContacts() {
       <PartnersTwoColumns>
         <div className={styles.leftCol}>
           <PartnerInputWithAdding
+            name={'email'}
             id={'email-partner'}
             type={'email'}
             title={'Email:'}
@@ -24,6 +24,7 @@ function PartnerContacts() {
 
           <PartnerInputWithAdding
             textAreaSmall
+            name={'description'}
             id={'sectionInfo-partner'}
             type={'text'}
             title={'Инфорамция раздела:'}
@@ -32,6 +33,7 @@ function PartnerContacts() {
         </div>
         <div className={styles.rightCol}>
           <PartnerInputWithAdding
+            name={'phone'}
             id={'phoneContacts-partner'}
             type={'phone'}
             title={'Контактный телефон:'}
